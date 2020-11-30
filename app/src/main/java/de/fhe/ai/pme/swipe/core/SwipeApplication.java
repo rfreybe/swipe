b08e04661d08103ebd18d5fbe8e16e69e83a52e1
JFuqX <john.reinecke99@gmail.com>
2020-12-01 00:34:43 +0100

    +added several ic´s +edited build.gradle and added dependencies +created activity_main.xml, main_toolbar.xml, main_nav_drawer.xml +first UI design, missing logic ... comming soon

--- app/src/main/java/de/fhe/ai/pme/swipe/core/SwipeApplication.java
+++ app/src/main/java/de/fhe/ai/pme/swipe/core/SwipeApplication.java
@@ -3,4 +3,5 @@ package de.fhe.ai.pme.swipe.core;
 import android.app.Application;
 
 public class SwipeApplication extends Application {
+
 }
--- app/src/main/java/de/fhe/ai/pme/swipe/view/MainActivity.java
+++ app/src/main/java/de/fhe/ai/pme/swipe/view/MainActivity.java
@@ -1,7 +1,19 @@
 package de.fhe.ai.pme.swipe.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG_LIFE_CYCLES = "LifecycleCallbacks";
+    private static final String LOG_TAG_EVENTS = "EventCallbacks";
 }
