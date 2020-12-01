8e7e088d93cc3a9b8f2e06c50ad62c39d0fe1ee2
JFuqX <john.reinecke99@gmail.com>
2020-12-01 16:43:01 +0100

    +added MainActivity.java methods for UI Control +fixed main_nav_drawer.xml activity_main.xml

--- app/src/main/java/de/fhe/ai/pme/swipe/view/MainActivity.java
+++ app/src/main/java/de/fhe/ai/pme/swipe/view/MainActivity.java
@@ -2,6 +2,8 @@ package de.fhe.ai.pme.swipe.view;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,8 +14,40 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import de.fhe.ai.pme.swipe.R;
 
-    private static final String LOG_TAG_LIFE_CYCLES = "LifecycleCallbacks";
-    private static final String LOG_TAG_EVENTS = "EventCallbacks";
+public class MainActivity extends AppCompatActivity {
+    //Initialize variable
+    DrawerLayout drawerLayout;
+
+    protected void OnCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Assign variable
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+    public void ClickMenu(View view){
+        //Open drawer
+        openDrawer(drawerLayout);
+    }
+
+    private static void openDrawer(DrawerLayout drawerLayout) {
+        //Opens drawer layout
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+    public void ClickLogo(View view){
+        //Close drawer
+        closeDrawer(drawerLayout);
+    }
+
+    private static void closeDrawer(DrawerLayout drawerLayout) {
+        //Closes drawer layout
+        //Check condition
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            //When drawer is open
+            //Close drawer
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
 }
